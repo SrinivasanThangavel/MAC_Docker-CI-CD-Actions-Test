97ae07ad72f201b6f45563fa7c4fea3931bee494
srinivasan.thangavel <srinivasan.thangavel@niit.com>
2023-07-12 09:43:44 +0530

    inital push

---
+++ src/main/java/com/stackroute/MedicalRecordsAccessControlService/MedicalRecordsAccessControlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.MedicalRecordsAccessControlService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordsAccessControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordsAccessControlServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackroute/MedicalRecordsAccessControlService/MedicalRecordsAccessControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.MedicalRecordsAccessControlService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordsAccessControlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
